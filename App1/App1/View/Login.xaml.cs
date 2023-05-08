8353b48766c3564293fb79c6a947c8bef521778a
Usuario <Usuario@CPD03-M19>
2023-05-08 09:36:59 -0300

    Adicionando Login colocando imagem arrumando interface inicial

--- App1/App1/View/Login.xaml.cs
+++ App1/App1/View/Login.xaml.cs
@@ -16,9 +16,51 @@ namespace App1.View
         {
             InitializeComponent();
         }
-        private void Button_Clicked(object sender, EventArgs e)
+
+
+
+        private void user_Clicked(object sender, EventArgs e)
+        {
+            string cpf = txt_cpf.Text;
+            string senha = txt_senha.Text;
+
+            if (string.IsNullOrEmpty(cpf) || string.IsNullOrEmpty(senha))
+            {
+                DisplayAlert("Erro de Login", "CPF e senha são obrigatórios.", "OK");
+                return;
+            }
+
+            // Verifica se o CPF e a senha inseridada é valida
+            bool isValidUser = CheckCredentials(cpf, senha);
+
+            if (isValidUser)
+            {
+                // Navegar para a próxima página ou fornecer acesso às funcionalidades protegidas pelo login
+                // Exemplo:
+                // Navigation.PushAsync(new HomePage());
+                // Não feita
+            }
+            else
+            {
+                DisplayAlert("Erro de Login", "CPF ou senha inválidos.", "OK");
+            }
+        }
+        private bool CheckCredentials(string cpf, string senha)
         {
 
+            if (cpf == "123.456.789-00" && senha == "password123")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void cadastro_Clicked(object sender, EventArgs e)
+        {
+            
         }
     }
 }
