4f777d519e724090819d88001746bba372721b2f
Tox <Usuario@CPD02-M01>
2023-05-19 09:25:07 -0300

    Tentando Fazer Registro Funcionar Com Sqlite bem basico (Odeio banco)...

--- App1/App1/View/cadastro.xaml.cs
+++ App1/App1/View/cadastro.xaml.cs
@@ -1,83 +1,65 @@
-﻿using SQLite;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-
+using App1.Banco; // Removi o "Banco" duplicado aqui
+using SQLite;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using static App1.Banco.Banco;
 
 namespace App1.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class cadastro : ContentPage
     {
-        string CPF;
-        string NOME;
-        string SENHA;
+        public string CPF { get; set; }
+        public string NOME { get; set; }
+        public string SENHA { get; set; }
 
-        private SQLiteConnection database;
         public cadastro()
         {
             InitializeComponent();
             logo.Source = ImageSource.FromResource("App1.Imagem.itauu.png");
-
-            
-            string databasePath = DependencyService.Get<Banco.Banco>().GetDatabasePath();
-            database = new SQLiteConnection(databasePath);
-            database.CreateTable<Usuario>(); // Cria a tabela do banco de dados se não existir
         }
-
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
             try
             {
-                App.Current.MainPage = new NavigationPage(new View.Login());
-            }
-            catch (Exception ex)
-            {
-                await DisplayAlert("Ops, ocorreu um erro", ex.Message, "OK");
-            }
-            Button botao = (Button)sender;
-            await botao.ScaleTo(1.2, 100, Easing.CubicOut);
-            await botao.ScaleTo(1, 100, Easing.CubicIn);
-            try
-            {
-                // Salva os dados do usuário no banco de dados
-                Usuario usuario = new Usuario
+                // Criando Cadastro Para Colocar Na "Login"
+                var novoCadastro = new cadastro
                 {
                     CPF = txt_cpf.Text,
-                    Nome = txt_nome.Text,
-                    Senha = txt_senha.Text
+                    NOME = txt_nome.Text,
+                    SENHA = txt_senha.Text
                 };
-                database.Insert(usuario);
+
+
+                // Salvar o cadastro no banco de dados
+                var databasePath = DependencyService.Get<IDatabasePath>().GetDatabasePath();
+                var context = new DatabaseContext(databasePath);
+                context.Insert(novoCadastro);
 
                 await DisplayAlert("Sucesso", "Usuário cadastrado com sucesso.", "OK");
-                App.Current.MainPage = new NavigationPage(new View.Login());
+                await Navigation.PopAsync(); //Volta para tela de login dps de se registrar
             }
             catch (Exception ex)
             {
                 await DisplayAlert("Ops, ocorreu um erro", ex.Message, "OK");
+                //Caso não funcione ele dara erro "provalmente" erro na instancia entre banco e o App1.Banco.Banco;
             }
         }
 
-        private async void Button_Clicked_1(object sender, EventArgs e)
+
+        private void Button_Clicked_1(object sender, EventArgs e)
         {
-            try
-            {
-                App.Current.MainPage = new NavigationPage(new View.Login());
-            }
-            catch (Exception ex)
-            {
-                await DisplayAlert("Ops, ocorreu um erro", ex.Message, "OK");
-            }
-            Button botao = (Button)sender;
-            await botao.ScaleTo(1.2, 100, Easing.CubicOut);
-            await botao.ScaleTo(1, 100, Easing.CubicIn);
+
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+
         }
     }
 }
