fc539c2a868e9623aad3c367689fc5d0738d409a
ToxSCin <Usuario@CPD02-M04>
2023-11-16 14:36:33 -0300

    aa

--- App1/App1/View/Menu.xaml.cs
+++ App1/App1/View/Menu.xaml.cs
@@ -17,28 +17,18 @@ namespace App1.View
     {
         public Menu()
         {
+
             InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
             anun.Source = ImageSource.FromResource("App1.Imagem.anun.png");
             ToxBanco.Source = ImageSource.FromResource("App1.Imagem.ToxBankk.png");
         }
 
-        private async void Saldo(object sender, EventArgs e)
-        {
-            try
-            {
-                App.Current.MainPage = new NavigationPage(new View.Saldo());
-            }
-            catch (Exception ex)
-            {
-                await DisplayAlert("Ops, ocorreu um erro", ex.Message, "OK");
-            }
-            Button botao = (Button)sender;
-            
-        }
+       
 
         
 
-        private async void Button_Clicked(object sender, EventArgs e)
+        private async void Saldo(object sender, EventArgs e)
         {
             try
             {
